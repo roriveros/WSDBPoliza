0fe713bdfe386ea629f25dc45e45e715e0ae8413
Rodrigo thrash <Rodrigo thrash@RODRIGOTHRASH>
2017-10-02 22:30:28 -0300

    conexion con WS lista

---
+++ WSDBPoliza/BDPoliza.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+namespace WSDBPoliza
+{
+    
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class BDPoliza : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataSet GetData()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=Rodrigothrash; Initial Catalog=rodrigoontour; user=c##pablo; pass=pablo1234;";
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM POLIZASEGURO", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
